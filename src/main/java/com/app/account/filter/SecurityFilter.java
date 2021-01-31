a7006066d3e93174d62ea963bfbc8d5774696946
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2021-01-31 21:27:55 +0100

    adding filter condition

--- src/main/java/com/app/account/filter/SecurityFilter.java
+++ src/main/java/com/app/account/filter/SecurityFilter.java
@@ -1,9 +1,13 @@
 package com.app.account.filter;
 
+import com.app.account.exception.ErrorDetails;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @Component
 public class SecurityFilter implements Filter {
@@ -13,6 +17,26 @@ public class SecurityFilter implements Filter {
         System.out.println("Here is my filter");
         System.out.println(request.getLocalName());
         System.out.println(request.getLocalAddr());
+        if(!request.getLocalAddr().equals("172.19.99.90")){
+            //ANY POJO CLASS
+            // ErrorResponse is a public return object that you define yourself
+            ErrorDetails errorResponse = new ErrorDetails();
+            errorResponse.setDate(new Date());
+            errorResponse.setMessage("Unauthorized Access");
+            errorResponse.setDetails("Access not authorized from this address");
+
+            byte[] responseToSend = restResponseBytes(errorResponse);
+            ((HttpServletResponse) response).setHeader("Content-Type", "application/json");
+            ((HttpServletResponse) response).setStatus(401);
+            response.getOutputStream().write(responseToSend);
+            return;
+        }
+
         chain.doFilter(request,response);
+    }
+
+    private byte[] restResponseBytes(ErrorDetails eErrorResponse) throws IOException {
+        String serialized = new ObjectMapper().writeValueAsString(eErrorResponse);
+        return serialized.getBytes();
     }
 }
