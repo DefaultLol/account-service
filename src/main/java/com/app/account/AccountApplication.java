fcb7641633f059e034329c9f859a4120b0f5291c
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2021-02-04 14:34:19 +0100

    adding

--- src/main/java/com/app/account/AccountApplication.java
+++ src/main/java/com/app/account/AccountApplication.java
@@ -1,9 +1,9 @@
 package com.app.account;
 
-import org.springframework.batch.core.Job;
+/*import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobLauncher;*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
@@ -13,18 +13,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class AccountApplication {
-    @Autowired
+    /*@Autowired
     private Job accountJob;
     @Autowired
-    JobLauncher jobLauncher;
+    JobLauncher jobLauncher;*/
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
     }
 
-    public void alimentationAccount() throws Exception{
+    /*public void alimentationAccount() throws Exception{
         JobParameters params = new JobParametersBuilder()
                 .addString("JobID", String.valueOf(System.currentTimeMillis()))
                 .toJobParameters();
         jobLauncher.run(accountJob, params);
-    }
+    }*/
 }
